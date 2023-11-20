aaa34478232b1e144e39556b319a894ad9ee0b79
anxi01 <tjdals2893@naver.com>
2023-11-20 19:11:00 +0900

    init

---
+++ LeetsGarden/src/main/java/com/example/leetsgarden/LeetsGardenApplication.java
@@ -0,0 +1,13 @@
+package com.example.leetsgarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetsGardenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetsGardenApplication.class, args);
+    }
+
+}
---
+++ LeetsGarden/src/test/java/com/example/leetsgarden/LeetsGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leetsgarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetsGardenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
