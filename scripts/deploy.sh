fdc393945838f49ca3d67b99563ba854727e5308
Leeul <92284769+rootTiket@users.noreply.github.com>
2023-11-23 18:42:32 +0900

    fix: 스크립트 파일에 application yml 경로 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ sudo docker rmi l2007889/leetsgarden:1.0
 sudo docker pull l2007889/leetsgarden:1.0
 
 # 도커 run
-sudo docker run -d -p 8080:8080 --name leetsgarden l2007889/leetsgarden:1.0
+sudo docker run -d -p 8080:8080 -v /home/ubuntu:/config --name leetsgarden l2007889/leetsgarden:1.0
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
