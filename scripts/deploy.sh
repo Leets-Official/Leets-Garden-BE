1d96c235c3804600d3404ff49df36e3069b7298a
Leeul <92284769+rootTiket@users.noreply.github.com>
2023-11-26 19:04:21 +0900

    fix: 권한 문제로 인해 기존 도커이미지 삭제가 안되는 현상 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 # 가동중인 awsstudy 도커 중단 및 삭제
-sudo docker ps -a -q --filter "name=leetsgarden" | grep -q . && docker stop leetsgarden && docker rm leetsgarden | true
+sudo docker ps -a -q --filter "name=leetsgarden" | grep -q . && sudo docker stop leetsgarden && sudo docker rm leetsgarden | true
 
 # 기존 이미지 삭제
 sudo docker rmi l2007889/leetsgarden:1.0
@@ -11,4 +11,4 @@ sudo docker pull l2007889/leetsgarden:1.0
 sudo docker run -d -p 8080:8080 -v /home/ubuntu:/config --name leetsgarden l2007889/leetsgarden:1.0
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
-docker rmi -f $(docker images -f "dangling=true" -q) || true
+sudo docker rmi -f $(sudo docker images -f "dangling=true" -q) || true
