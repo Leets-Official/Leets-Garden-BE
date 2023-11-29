aa995c055e84be59cfd1d0b2b93def8f27798335
anxi01 <tjdals2893@naver.com>
2023-11-29 17:58:03 +0900

    HOTFIX : fix CORS

--- src/main/java/com/example/leetsgarden/LeetsGardenApplication.java
+++ src/main/java/com/example/leetsgarden/LeetsGardenApplication.java
@@ -2,13 +2,26 @@ package com.example.leetsgarden;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class LeetsGardenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LeetsGardenApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
 }
