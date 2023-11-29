45a81289edb45772f6cd3aa7719368de08518f7c
노정완 <jeongwan.noh@gmail.com>
2023-11-29 15:24:05 +0900

    fix: final 적용을 통한 값 변경 방지

--- src/main/java/com/example/leetsgarden/domain/Role.java
+++ src/main/java/com/example/leetsgarden/domain/Role.java
@@ -7,10 +7,13 @@ import java.util.List;
 public enum Role {
     ADMIN("ROLE_ADMIN,ROLE_USER"),
     USER("ROLE_USER");
-    private String role;
+
+    private final String role;
+
     Role(String role) {
         this.role = role;
     }
+
     public List<String> value() {
         return Collections.unmodifiableList(Arrays.asList(this.role.split(",")));
 
