ae6d140f0660be7cc514318cd32018ff840ad0f4
노정완 <jeongwan.noh@gmail.com>
2023-12-03 00:34:07 +0900

    HOTFIX: fix CORS

--- src/main/java/com/example/leetsgarden/security/WebConfig.java
+++ src/main/java/com/example/leetsgarden/security/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://3.39.24.69:8080", "http://3.39.24.69:3000")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(false)
                 .allowedHeaders("*")
