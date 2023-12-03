12bbb1a8e919d8009a40b7c20d916a3e746414d3
rootTiket <l20078829@gmail.com>
2023-12-03 15:53:40 +0900

    HOTFIX: CustomCorsFilter 수정

--- src/main/java/com/example/leetsgarden/security/CustomCorsFilter.java
+++ src/main/java/com/example/leetsgarden/security/CustomCorsFilter.java
@@ -10,7 +10,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Configuration
 public class CustomCorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)throws ServletException, IOException {
