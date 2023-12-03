020f9681149ef5e84efc4078b3da9296b32312f5
노정완 <jeongwan.noh@gmail.com>
2023-12-03 16:32:22 +0900

    HOTFIX: fix CORS

--- src/main/java/com/example/leetsgarden/security/WebConfig.java
+++ src/main/java/com/example/leetsgarden/security/WebConfig.java
@@ -2,15 +2,17 @@ package com.example.leetsgarden.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://3.39.24.69:8080/register", "http://localhost:3000", "http://localhost:8080", "http://3.39.24.69:8080", "http://3.39.24.69:3000")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
                 .allowCredentials(true)
                 .allowedHeaders("*")
                 .exposedHeaders("*")
