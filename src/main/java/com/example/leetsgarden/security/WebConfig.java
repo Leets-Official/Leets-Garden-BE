0ea794e38a45d48dec0a6290f1516bc557849481
노정완 <jeongwan.noh@gmail.com>
2023-12-03 14:05:29 +0900

    HOTFIX : add PATCH Method to CORS

--- src/main/java/com/example/leetsgarden/security/WebConfig.java
+++ src/main/java/com/example/leetsgarden/security/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://3.39.24.69:8080", "http://3.39.24.69:3000")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(false)
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
