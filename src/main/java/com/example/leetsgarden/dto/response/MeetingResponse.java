a73ce5bad2d79fe6ad8c3c3fbf4b84906ee7f6f6
노정완 <jeongwan.noh@gmail.com>
2023-12-04 09:35:52 +0900

    feat: 특정 미팅 조회시 참여하는 사용자 이름을 반환

--- src/main/java/com/example/leetsgarden/dto/response/MeetingResponse.java
+++ src/main/java/com/example/leetsgarden/dto/response/MeetingResponse.java
@@ -1,12 +1,12 @@
 package com.example.leetsgarden.dto.response;
 
 import com.example.leetsgarden.domain.Meeting;
-import com.example.leetsgarden.domain.User;
 import com.example.leetsgarden.domain.UserMeeting;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
@@ -20,7 +20,7 @@ public class MeetingResponse {
 
     private String meetingDay;
 
-    private List<User> userList;
+    private List<String> userList;
 
     public static MeetingResponse from(Meeting meeting) {
         MeetingResponse meetingResponse = MeetingResponse.builder()
@@ -33,8 +33,8 @@ public class MeetingResponse {
         List<UserMeeting> userMeetings = meeting.getUserMeetings();
 
         meetingResponse.userList = userMeetings.stream()
-                .map(UserMeeting::getUser)
-                .toList();
+                .map(userMeeting -> userMeeting.getUser().getName())
+                .collect(Collectors.toList());
 
         return meetingResponse;
     }
